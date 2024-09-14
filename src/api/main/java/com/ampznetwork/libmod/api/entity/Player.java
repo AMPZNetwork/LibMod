7352528f4022cef9a1965aaf89a5f063e7c5f0a9
burdoto <kaleidox@comroid.org>
2024-09-14 19:19:04 +0200

    deprecate playerdata in favor of player

--- src/api/main/java/com/ampznetwork/libmod/api/entity/Player.java
+++ src/api/main/java/com/ampznetwork/libmod/api/entity/Player.java
@@ -6,19 +6,34 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Singular;
 import lombok.experimental.SuperBuilder;
+import org.comroid.annotations.Doc;
 import org.comroid.api.Polyfill;
 import org.comroid.api.net.REST;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapKeyColumn;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import java.net.InetAddress;
+import java.time.Instant;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
 
+import static java.time.Instant.*;
+import static org.comroid.api.Polyfill.*;
 import static org.comroid.api.net.REST.Method.*;
 
 @Data
@@ -38,7 +53,20 @@ public class Player extends DbObject {
      */
     public static final EntityType<Player, Builder<Player, ?>> TYPE
             = Polyfill.uncheckedCast(new EntityType<>(Player::builder, null, Player.class, Builder.class));
-    public static       BiConsumer<UUID, String>    CACHE_NAME = null;
+    public static       BiConsumer<UUID, String>          CACHE_NAME         = null;
+    public static final Comparator<Map.Entry<?, Instant>> MOST_RECENTLY_SEEN = Comparator.comparingLong(e -> e.getValue().toEpochMilli());
+    @Singular
+    @ElementCollection
+    @Column(name = "seen")
+    @MapKeyColumn(name = "name")
+    @CollectionTable(name = "playerdata_names", joinColumns = @JoinColumn(name = "id"))
+    Map<@Doc("name") String, @Doc("seen") Instant> knownNames = new HashMap<>();
+    @Singular
+    @ElementCollection
+    @Column(name = "seen")
+    @MapKeyColumn(name = "ip")
+    @CollectionTable(name = "playerdata_ips", joinColumns = @JoinColumn(name = "id"))
+    Map<@Doc("ip") String, @Doc("seen") Instant>   knownIPs   = new HashMap<>();
 
     public static CompletableFuture<UUID> fetchId(String name) {
         var future = REST.get("https://api.mojang.com/users/profiles/minecraft/" + name)
@@ -65,5 +93,35 @@ public class Player extends DbObject {
         return Optional.ofNullable(name)
                 .map(CompletableFuture::completedFuture)
                 .orElseGet(() -> fetchUsername(id));
+    }
+
+    @JsonIgnore
+    public Optional<String> getLastKnownName() {
+        return knownNames.entrySet().stream()
+                .max(Player.MOST_RECENTLY_SEEN)
+                .map(Map.Entry::getKey);
+    }
+
+    @JsonIgnore
+    public Optional<String> getLastKnownIp() {
+        return knownIPs.entrySet().stream()
+                .max(Player.MOST_RECENTLY_SEEN)
+                .map(Map.Entry::getKey);
+    }
+
+    @Contract(value = "!null->this", pure = true)
+    public Player pushKnownName(String name) {
+        var map = getKnownNames();
+        map = new HashMap<>(map);
+        map.compute(name, ($0, $1) -> now());
+        return this;
+    }
+
+    @Contract(value = "!null->this", pure = true)
+    public Player pushKnownIp(InetAddress ip) {
+        var map = getKnownIPs();
+        map = new HashMap<>(map);
+        map.compute(ip2string(ip), ($0, $1) -> now());
+        return this;
     }
 }
