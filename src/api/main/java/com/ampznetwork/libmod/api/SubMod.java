77f251394ad362749231eb59ca41c3aa273e90c1
burdoto <kaleidox@comroid.org>
2024-10-06 11:05:56 +0200

    fix initialization

--- src/api/main/java/com/ampznetwork/libmod/api/SubMod.java
+++ src/api/main/java/com/ampznetwork/libmod/api/SubMod.java
@@ -5,10 +5,14 @@ import com.ampznetwork.libmod.api.interop.database.IEntityService;
 import org.comroid.api.attr.Named;
 import org.comroid.api.func.Specifiable;
 import org.comroid.api.func.util.Command;
+import org.comroid.api.func.util.Streams;
 
+import java.util.Arrays;
 import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Stream;
 
-public interface SubMod extends Specifiable<SubMod> {
+public interface SubMod extends Specifiable<SubMod>, Command.ContextProvider {
     LibMod getLib();
 
     Command.Manager getCmdr();
@@ -25,6 +29,12 @@ public interface SubMod extends Specifiable<SubMod> {
 
     default boolean hasCapability(Capability capability) {
         return getCapabilities().contains(capability);
+    }
+
+    @Override
+    default Stream<Object> expandContext(Object... context) {
+        return Arrays.stream(context)
+                .flatMap(Streams.expand(it -> it instanceof UUID id ? getLib().getPlayerAdapter().getPlayer(id).stream() : Stream.empty()));
     }
 
     enum Capability implements Named {
